458e4783cd24ce3aa015f5180450e4f33ea2e245
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-09-29 01:35:34 +0600

    [navbar and fake data create]

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className='container-body'>
+            <div>
+
+            </div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Home;
