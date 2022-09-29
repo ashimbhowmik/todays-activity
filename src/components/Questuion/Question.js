14fdd8f447a527ba30123c99015419113a30f137
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-09-29 23:40:56 +0600

    [qustion update]

--- src/components/Questuion/Question.js
+++ src/components/Questuion/Question.js
@@ -23,7 +23,7 @@ const Question = () => {
                     <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
                         <svg aria-hidden="true" className="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                     </span>
-                    <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.2.2</h3>
+                    <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">What does useEffect do ?</h3>
                     <p className="text-base font-normal text-gray-500 dark:text-gray-400">If we perform a side effect directly in our component body, it gets in the way of our React component's rendering. Side effects should be separated from the rendering process. If we need to perform a side effect, it should strictly be done after our component renders. This is what useEffect gives us .In short, useEffect is a tool that lets us interact with the outside world but not affect the rendering or performance of the component that it's in.</p>
                 </li>
             </ol>
